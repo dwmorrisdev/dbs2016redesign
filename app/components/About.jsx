306815775db20cd2f82c790e878f90f532be590a
Dustin Morris <dwmorris.biz@outlook.com>
2016-10-27 21:50:10 -0500

    additional content

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,15 +1,35 @@
 var React = require('react');
 
-
 var About = (props) => {
     return (
         <div >
-          <h1 className="text-center page-title">Who I am</h1>
-          <div className="container text-center">
-            <h3>Hi, I'm Dustin</h3>
-            <p>About me and design by sparticus.</p>
-          </div>
-          <div className="container text-center"><p>insert image here</p></div>
+            <h1 className="text-center page-title">Who I am</h1>
+            <div className="row">
+                <div className="text-center medium-6">
+                    <h3>Hi, I'm Dustin</h3>
+                    <p>About me and design by sparticus.</p>
+                    <span>
+                        <h3>Links</h3>
+                        <ul>
+                            <li>
+                                <a href="https://github.com/dwmorrisdev/dbs2016redesign" target="_blank">designbysparticus source on Github</a>
+                            </li>
+                            <li>
+                                <a href="https://facebook.github.io/react/" target="_blank">React</a>
+                            </li>
+                            <li><a href="https://www.twitter.com/thedeadsparticus" target="_blank">Twitter</a></li>
+                            <li><a href="https://www.linkedin.com/in/dustin-morris-a4773283" target="_blank">LinkedIn</a></li>
+                            <li><a href="https://www.facebook.com/deadperspective" target="_blank">Dead Perspective</a></li>
+                            <li>Photos by
+                                <a href="https://unsplash.com/" target="_blank"> Unsplash</a>
+                            </li>
+                        </ul>
+                    </span>
+                </div>
+                <div className="medium-6">
+                    <p>insert image here</p>
+                </div>
+            </div>
         </div>
     );
 };
